ecfcae8a02679e822a9bfb75fef74e7c47e13ade
stwebyy <stweb.yy@gmail.com>
2018-07-28 12:09:09 +0900

    設定ファイルの修正

--- app/settings.py
+++ app/settings.py
@@ -158,3 +158,9 @@ EMAIL_USE_TLS = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+ import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
