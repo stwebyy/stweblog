661bcaa4f6c8e79dec1d5a1819c738067f98d3b4
stwebyy <stweb.yy@gmail.com>
2018-12-08 11:43:12 +0900

    React環境実装

--- app/settings.py
+++ app/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'pure_pagination',
     'test_list',
     'martor',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -200,3 +203,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
