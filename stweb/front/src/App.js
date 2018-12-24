15bab59cdc11a5044c9568c18d99bb2998cfc642
stwebyy <stweb.yy@gmail.com>
2018-12-24 13:30:25 +0900

    Reactで表示するページにcssとjsを反映

--- stweb/front/src/App.js
+++ stweb/front/src/App.js
@@ -2,54 +2,20 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import './App.css';
 import axios from 'axios';
+import Home from './Home';
+import Header from './header';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-        todos: []
+
     };
   }
-
-    componentDidMount() {
-        this.getTodos();
-    }
-
-    getTodos() {
-        axios
-            .get('http://localhost:8000/api/articles/')
-            .then(res => {
-                this.setState({ todos: res.data.results });
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-    // render() {
-    //     return (
-    //       <div>
-    //             {this.state.todos.map(item => (
-    //                  <ul key={item.id}>
-    //                     <li>{item.title}</li>
-    //                     <li>{item.text}</li>
-    //                  </ul>
-    //             ))}
-    //       </div>
-    //     );
-    // }
     render() {
     return (
       <div>
-        <h1>テスト</h1>
-          {this.state.todos.map( todos => (
-            <div key={todos.id}>
-            <Link to='/detail/:id'>
-            <li>{todos.title}</li>
-            <li>{todos.text}</li>
-            </Link>
-            </div>
-          ))}
+        <Home />
       </div>
     );
   }
